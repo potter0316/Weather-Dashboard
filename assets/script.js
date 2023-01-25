d48e8d4b24d95f9c157e563ada6c833fef33d034
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-01-24 23:40:00 -0600

    added readme major changes in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,103 +1,160 @@
+//Converts kelvin into fahrenheit
+let kToF = function (kelvinTemp) {
+  const celsius = kelvinTemp - 273;
+  const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+  return fahrenheit;
+}
 $(document).ready(function () {
-    //search button
-    $("#search-button").on("click", function () {
-        let searchCity = $("#search-value").val();
-        $("#search-value").val("");
-        weatherFunction(searchCity);
-        weatherForecast(searchCity);
-    });
-  
-    //search button enter key feature. 
-    $("#search-button").keypress(function (event) {
-        let keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode === 13) {
-            weatherFunction(searchTerm);
-            weatherForecast(searchTerm);
-        }
-    });
+  let api = "15b46b4024aa9c9876a3ca0520fd79ff";
+  //search button
+  $("#search-button").on("click", function () {
+    let citySearch = $("#search-value").val();
+    $("#search-value").val("");
+    weatherFunction(citySearch);
+    weatherForecast(citySearch);
+  });
 
-    let 
-
-    let gotPosition = function (pos) {
-        let lat = pos.coords.latitude;
-        let long = pos.coords.longitude;
-        console.log(lat);
-        console.log(long);
-        getForecast(lat, long);
+  //search button enter key feature.
+  $("#search-button").keypress(function (event) {
+    let keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode === 20) {
+      weatherFunction(citySearch);
+      weatherForecast(citySearch);
     }
+  });
 
-    let getForecast = function (lat, long) {
-        let url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + long + "&appid=15b46b4024aa9c9876a3ca0520fd79ff"
-        getWeatherText(url);
-    
-    }
+  //local storage
+  let storage = JSON.parse(localStorage.getItem("local")) || [];
 
-    async function getWeatherText(url) {
-        let weatherObject = await fetch(url);
-        let weatherText = await weatherObject.text();
-        parseWeather(weatherText);
-    }
+  if (storage.length > 0) {
+    weatherFunction(storage[storage.length - 1]);
+  }
+  //creates storage
+  for (let i = 0; i < storage.length; i++) {
+    createRow(storage[i]);
+  }
 
-    let parseWeather = function (weatherText) {
-        let weatherJSON = JSON.parse(weatherText);
-        let dailyForecast = weatherJSON.daily;
-        for (x = 0; x < dailyForecast.length; x++) {
-            let day = dailyForecast[x];
-            let today = new Date().getDay() + x;
-            if (today > 6) {
-                today = today - 7;
-            }
-            let dayOfWeek = getDayOfWeek(today);
-            let icon = day.weather[0].icon;
-            let highTemp = kToF(day.temp.max);
-            let lowTemp = kToF(day.temp.min);
-            let humidity = day.humidity;
-            let windSpeed = day.wind_gust;
-            let windGust = day.wind_gust;
-            displayWeatherDay(dayOfWeek, icon, highTemp, lowTemp, humidity, windSpeed, windGust);
-        }
-    }
+  //local storage of the last cities searched
+  function createRow(text) {
+    let cityList = $("<li>").addClass("list-group-item").text(text);
+    $(".local").append(cityList);
+  }
 
-    let displayWeatherDay = function (dayOfWeek, icon, highTemp, lowTemp, humidity, windSpeed, windGust) {
-        let out = "<div class='weatherDay'><img src='http://openweathermap.org/img/wn/" + icon + ".png'/>";
-        out += "<h2>" + dayOfWeek + "<h2>";
-        out += "<p>" + highTemp + "F</p>";
-        out += "<p>" + lowTemp + "F</p>";
-        out += "<p>" + humidity + "%</p>";
-        out += "<p>" + Math.round(windSpeed) + 'mph</p>';
-        document.getElementById("forecast").innerHTML += out;
-    }
+  //listener for list item on click function
+  $(".local").on("click", "li", function () {
+    weatherFunction($(this).text());
+    weatherForecast($(this).text());
+  });
 
-    let getDayOfWeek = function (dayNum) {
-        let weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
+  function weatherFunction(citySearch) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        citySearch +
+        "&appid=" +
+        api,
+    }).then(function (data) {
+        if (storage.indexOf(citySearch) === -1) {
+        storage.push(citySearch);
+        localStorage.setItem("local", JSON.stringify(storage));
+        createRow(citySearch);
+      }
+      $("#today").empty();
 
-        return (weekday[dayNum]);
-    }
+      let city = $("<h3>").text(
+        data.name + " (" + new Date().toLocaleDateString() + ")"
+      );
+      let img = $("<img>").attr(
+        "src",
+        "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+      );
 
-    let kToF = function (kelvinTemp) {
-        const celsius = kelvinTemp - 273;
-        const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
-        return fahrenheit
-    }
+      let card = $("<div>");
+      let cardBody = $("<div>");
+      let wind = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
+      let humid = $("<p>").text("Humidity: " + data.main.humidity + " %");
+      let temp = $("<p>").text("Temperature: " + kToF(data.main.temp) + " F");
+      let lon = data.coord.lon;
+      let lat = data.coord.lat;
 
-    let timestampToTime = function (timeStamp) {
-        let date = new Date(timeStamp * 1000);
-        let hours = date.getHours();
-        let minutes = "";
-        if (date.getMinutes() < 10) {
-            minutes = "0" + date.getMinutes();
+      $.ajax({
+        type: "GET",
+        url:
+          "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+          api +
+          "&lat=" +
+          lat +
+          "&lon=" +
+          lon,
+      }).then(function (response) {
+        let uvResponse = response.value;
+        let btn = $("<span>").addClass("btn").text(uvResponse);
+
+        if (uvResponse < 3) {
+          btn.addClass("btn-success");
+        } else if (uvResponse < 7) {
+          btn.addClass("btn-warning");
         } else {
-            minutes = date.getMinutes();
+          btn.addClass("btn-danger");
         }
-        return hours + ":" + minutes;
-    }
 
-    navigator.geolocation.getCurrentPosition(gotPosition);
-})
+        cardBody.append(uvIndex);
+        $("#today .card-body").append(uvIndex.append(btn));
+      });
+
+      // merge and add to page
+      city.append(img);
+      cardBody.append(city, temp, wind, humid);
+      card.append(cardBody);
+      $("#today").append(card);
+    });
+  }
+  // function weatherForecast(citySearch)
+  function weatherForecast(citySearch) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        citySearch +
+        "&appid=9f112416334ce37769e5c8683b218a0d&units=imperial",
+    }).then(function (data) {
+      $("#weatherDay")
+        .html('<h4 class="mt-3">5-Day Forecast:</h4>')
+        .append('<div class="row">');
+
+      // 5 Day Forecast
+
+      for (let i = 0; i < data.list.length; i++) {
+        if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+          let fiveDay = $("<h3>").text(
+            new Date(data.list[i].dt_txt).toLocaleDateString()
+          );
+          let imgFive = $("<img>").attr(
+            "src",
+            "https://openweathermap.org/img/w/" +
+              data.list[i].weather[0].icon +
+              ".png"
+          );
+          let colFive = $("<div>").addClass("weatherDay");
+          let cardFive = $("<div>").addClass("weatherDay");
+          let cardBodyFive = $("<div>");
+          let humidFive = $("<p>").text(
+            "Humidity: " + data.list[i].main.humidity + "%"
+          );
+          let tempFive = $("<p>").text(
+            "Temperature: " + data.list[i].main.temp + " °F"
+          );
+
+          colFive.append(
+            cardFive.append(
+              cardBodyFive.append(fiveDay, imgFive, tempFive, humidFive)
+            )
+          );
+   
+          $("#weatherDay .row").append(colFive);
+        }
+      }
+    });
+  }
+});
